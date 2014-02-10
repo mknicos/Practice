433eb715f263de019823830c090ea3ed2192c059
Matt Knicos <mknicos@gmail.com>
2014-02-09 19:24:43 -0600

    Array Addition

--- 2014-02-09-CodingChallenges/public/js/source/coderbyte.js
+++ 2014-02-09-CodingChallenges/public/js/source/coderbyte.js
@@ -1,7 +1,9 @@
-/*exported longestWord */
+/*exported arrayAddition, longestWord */
 
 function longestWord(sen) {
   'use strict';
+//Function will return the longest word in a string
+
   sen = sen.split(' ');
   var max = 0,
       longest = '',
@@ -17,6 +19,27 @@ function longestWord(sen) {
   sen = longest;
   return sen;
 }
-   
-// keep this function call here 
-// to see how to enter arguments in JavaScript scroll down
+
+function arrayAddition(arr){
+  'use strict';
+//Function will return true if all the numbers besides the largest
+//can add up to the largest
+
+  var largestNum = 0, newArray = [];
+//Find largest number
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] > largestNum){
+      largestNum = arr[i];
+    }
+  }
+//Make an array of all numbers but the largest number
+  for(var j = 0; j < arr.length; j ++){
+    if(arr[i] !== largestNum){
+      newArray.push(arr[i]);
+    }
+  }
+//see if it's possible to add up to the largest numbers using the new Array
+  for(var k = 0; k < newArray.length; k ++){
+    //need to finish code here
+  }
+}
--- 2014-02-09-CodingChallenges/public/js/source/test.js
+++ 2014-02-09-CodingChallenges/public/js/source/test.js
@@ -1,4 +1,4 @@
-/* global test, deepEqual, longestWord :false */
+/* global test, deepEqual, longestWord, arrayAddition :false */
 
 'use strict';
 
@@ -8,3 +8,12 @@ test('longest word', function(){
   deepEqual(longestWord(string),'string', 'the longest word in the string should be string');
 });
 
+test('Array Addition', function(){
+  var numArray = [4,6,23,10,1,3];
+  var numArray2 = [5,7,16,1,2];
+  var numArray3 = [3,5,-1,8,12];
+
+  deepEqual(arrayAddition(numArray), true, 'the other numbers should be able to add up to the largest num');
+  deepEqual(arrayAddition(numArray2), true, 'the longest word in the string should be string');
+  deepEqual(arrayAddition(numArray3), true, 'the other numbers should be able to add up to the largest num');
+});
